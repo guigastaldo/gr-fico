cca90f72ece9cffad411ebbfdc03f7006ceaaaa0
guigastaldo <106599090+guigastaldo@users.noreply.github.com>
2024-09-30 11:46:56 -0300

    Add files via upload

---
+++ grafico.js
@@ -0,0 +1,18 @@
+const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
+const graficoPizza = new Chart(ctxPizza, {
+    type: 'pie',
+    data: {
+        labels: ['Todos os dias', 'Raramente', 'As vezes', 'Nunca'],
+        datasets: [{
+            label: 'Alimentação Saudável',
+            data: [66, 16, 16, 0],
+            backgroundColor: [
+                '#F05454', '#30475E', '#DDDDDD', '#222831'
+            ],
+            borderColor: '#DDDDDD',
+            bordarWidth: 1
+        }]
+    },
+    
+
+}
